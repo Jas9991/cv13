5c12c6a69dcee5a51090a18ef7eb9e8a07d5808d
Jas9991 <62826563+Jas9991@users.noreply.github.com>
2023-02-05 13:00:34 +0530

    touch

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ var started = false;
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+$(document).on('touchstart' , function (event) {
+  if (event.key === "a") {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  }
+});
+
 $(document).keydown(function (event) {
   if (event.key === "a") {
     if (!started) {
